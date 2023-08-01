40a4603a6e4480f441f8f3a1403b2e814146ba74
Farid Alam <faridalam62@gmail.com>
2023-08-01 22:58:06 +0600

    Added header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Icon } from "@iconify/react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="container mx-auto flex justify-between items-center h-20">
+        <div>
+          <Link to="/">
+            <img src="/logo.png" />
+          </Link>
+        </div>
+        <div className="space-x-4 text-base">
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div className="flex gap-3 items-center justify-end">
+          <img src="/Profile.png" className="w-8" />
+          <img src="/Cart.png" className="w-6" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
